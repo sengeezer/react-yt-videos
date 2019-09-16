5508e401abe4e4723a13e202268f8d37f315cb0d
Fabio Albertin <albertin@gmail.com>
2019-09-16 23:59:05 +0100

    Finished lecture 120

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../api/youtube';
+import VideoList from './VideoList';
 
 class App extends Component {
-  onTermSubmit = term => {
-    youtube.get('/search', {
+  state = { videos: [] };
+
+  onTermSubmit = async term => {
+    const response = await youtube.get('/search', {
       params: {
         q: term
       }
     });
+
+    this.setState({ videos: response.data.items });
   }
   render() {
     return (
       <div className="App ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
---
+++ src/components/VideoList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import VideoItem from './VideoItem';
+
+const VideoList = ({ videos }) => {
+  const videoList = videos.map(video => {
+    return <VideoItem video={video} key={video.id.videoId} />
+  });
+
+  return (
+    <div>{videoList}</div>
+  );
+};
+
+export default VideoList;
